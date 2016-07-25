94a2688beb4376f9d80f091942d02c4202f0e41a
aiywatch <aiycp35@gmail.com>
2016-07-25 22:51:01 +0000

    refactor function showResult

--- game2.js
+++ game2.js
@@ -13,27 +13,29 @@ function result(user_n, rand_n, bet){
   }
 }
 
+function showResult(){
+  var bet = $("#bet").val();
+  var guess = $("#guess").val();
+  //alert(bet + guess);
+
+  var rand_n = Math.floor(Math.random() * 10) + 1;
+  $("ol").append('<li> You bet ' + bet + 
+    '<br>you guess number ' + guess + ' the right number is ' + rand_n +
+    '<br><h3>' + result(guess, rand_n, bet) + '</h3>' + 
+    '<br> your total bankroll is ' + bankroll + '</li>');
+
+  $("#bankroll").text(bankroll);
+
+  if(bankroll <= 0){
+    $('#regame').html(restart_btn);
+    $('#game').fadeOut();
+  }
+}
+
 $(document).ready(function(){
   $("#bankroll").text(bankroll);
 
-  $("#submit").click(function(){
-    var bet = $("#bet").val();
-    var guess = $("#guess").val();
-    //alert(bet + guess);
-
-    var rand_n = Math.floor(Math.random() * 10) + 1;
-    $("ol").append('<li> You bet ' + bet + 
-      '<br>you guess number ' + guess + ' the right number is ' + rand_n +
-      '<br><h3>' + result(guess, rand_n, bet) + '</h3>' + 
-      '<br> your total bankroll is ' + bankroll + '</li>');
-
-    $("#bankroll").text(bankroll);
-
-    if(bankroll <= 0){
-      $('#regame').html(restart_btn);
-      $('#game').fadeOut();
-    }
-  });
+  $("#submit").click(showResult);
 
   $('#regame').click('button', function(){
     location.reload();
